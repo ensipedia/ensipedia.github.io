fe28b1ed1e935fed5dbe827de05256cdbb3588cc
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2024-11-27 19:56:01 +0300

    perf(site): lazy load items

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { onMount } from "svelte";
+  import Lazy from "svelte-lazy";
   import {
     Card,
     CircularProgressIndeterminate,
@@ -8,7 +10,6 @@
   } from "m3-svelte";
   
   let data;
-  let dataPromise = getData();
   async function getData() {
     const response = await fetch("./data.json");
     data = await response.json();
@@ -19,39 +20,38 @@
   let query = "";
   let filtered;
   $: filtered = data?.[category]?.filter?.(w => w.toLowerCase().includes(query.toLowerCase())) ?? [];
+  
+  onMount(() => getData());
 </script>
 
-<div style:text-align="center">
-  <h1>Ensipedia</h1>
+<Lazy>
+  <div style:text-align="center">
+    <h1>Ensipedia</h1>
   
-  <TextFieldOutlined
-    bind:value={query}
-    name="Search" />
+    <TextFieldOutlined
+      bind:value={query}
+      name="Search" />
   
-  <SegmentedButtonContainer>
-    <input type="radio" id="words" value="words" bind:group={category} />
-    <SegmentedButtonItem input="words">Words</SegmentedButtonItem>
-    <input type="radio" id="verbs" value="verbs" bind:group={category} />
-    <SegmentedButtonItem input="verbs">Verbs</SegmentedButtonItem>
-  </SegmentedButtonContainer>
-  {#if data != null}
-    <br><br><p1>{
-      filtered.length ? `${filtered.length} items shown` : "No matches"
-    }</p1>
-  {/if}
-</div>
-
-<div style:text-align="center">
-  {#await dataPromise}
-    <br><CircularProgressIndeterminate />
-  {:then}
-    {#each filtered as word}
-      <br>
-      <Card type="outlined">
-        {word}
-      </Card>
-    {/each}
-  {:catch error}
-    <p style="color: red">Error: {error.message}</p>
-  {/await}
-</div>
+    <SegmentedButtonContainer>
+      <input type="radio" id="words" value="words" bind:group={category} />
+      <SegmentedButtonItem input="words">Words</SegmentedButtonItem>
+      <input type="radio" id="verbs" value="verbs" bind:group={category} />
+      <SegmentedButtonItem input="verbs">Verbs</SegmentedButtonItem>
+    </SegmentedButtonContainer>
+    {#if data != null}
+      <br><br><p1 style:text-align="left">{
+        filtered.length ? `${filtered.length} items shown` : "No matches"
+      }</p1>
+    {/if}
+    
+    {#if data == null}
+      <br><CircularProgressIndeterminate />
+    {:else}
+      <div style:text-align="left">
+        {#each filtered as word}
+          <br><Card type="outlined">{word}</Card>
+        {/each}
+      </div>
+    {/if}
+  </div>
+</Lazy>
