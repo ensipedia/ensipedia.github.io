352770b84454eb63160f834ed003ad05bab00495
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2024-11-27 22:18:02 +0300

    feat(site): topbar, query param, design improvements

---
+++ src/lib/components/TopBar.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let title = "Ensipedia";
+  export let icon = "favicon.png";
+</script>
+
+<div style:text-align="center" style:margin-bottom="8px">
+  <img src="favicon.png" style:width="64px" style:vertical-align="middle" style:margin-right="8px">
+  <p1 style:font-size="30px" style:vertical-align="middle">Ensipedia</p1>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { page } from "$app/stores";
   import { onMount } from "svelte";
   import Lazy from "svelte-lazy";
   import {
@@ -8,26 +9,24 @@
     SegmentedButtonItem,
     TextFieldOutlined
   } from "m3-svelte";
+  import TopBar from "$lib/components/TopBar.svelte";
   
   let data;
-  async function getData() {
-    const response = await fetch("./data.json");
-    data = await response.json();
-    return data;
-  }
-  
-  let category = "words";
+  let category = $page.url.searchParams.get("category") || "words";
   let query = "";
   let filtered;
   $: filtered = data?.[category]?.filter?.(w => w.toLowerCase().includes(query.toLowerCase())) ?? [];
   
-  onMount(() => getData());
+  onMount(async () => {
+    const response = await fetch("./data.json");
+    data = await response.json();
+  });
 </script>
 
 <Lazy>
   <div style:text-align="center">
-    <h1>Ensipedia</h1>
-  
+    <TopBar/>
+    
     <TextFieldOutlined
       bind:value={query}
       name="Search" />
@@ -39,19 +38,21 @@
       <SegmentedButtonItem input="verbs">Verbs</SegmentedButtonItem>
     </SegmentedButtonContainer>
     {#if data != null}
-      <br><br><p1 style:text-align="left">{
+      <br><p1 style:text-align="left">{
         filtered.length ? `${filtered.length} items shown` : "No matches"
       }</p1>
     {/if}
     
-    {#if data == null}
-      <br><CircularProgressIndeterminate />
-    {:else}
-      <div style:text-align="left">
+    <div style:margin="8px 0px 8px 0px">
+      {#if data == null}
+        <CircularProgressIndeterminate />
+      {:else}
         {#each filtered as word}
-          <br><Card type="outlined">{word}</Card>
+          <div style:text-align="left" style:margin="8px 0px 8px 0px">
+            <Card type="outlined">{word}</Card>
+          </div>
         {/each}
-      </div>
-    {/if}
+      {/if}
+    </div>
   </div>
 </Lazy>
