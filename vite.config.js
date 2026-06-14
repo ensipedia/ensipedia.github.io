cab42bbad9ee46bd19540dd881d92aecea55c460
alieRN <45766489+aliernfrog@users.noreply.github.com>
2026-06-14 21:23:51 +0300

    chore: Update to Vite 8

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { functionsMixins } from "vite-plugin-functions-mixins";
 
 export default defineConfig({
 	plugins: [
-	  sveltekit(),
+	  sveltekit({
+	    adapter: adapter({
+		    pages: "build",
+		    assets: "build",
+		    fallback: undefined,
+		    precompress: false,
+		    strict: true
+		  })
+	  }),
 	  functionsMixins({ deps: ["m3-svelte"] })
 	]
 });
